33964272ffadc71b53278fbebf55b5b30acf503f
rajindersingh98 <rajindersingh98@gmail.com>
2020-04-20 17:07:25 +0530

    Adding file to run test on Selenium Hub and Node

--- src/test/java/com/jenkins/newtest/BaseTest.java
+++ src/test/java/com/jenkins/newtest/BaseTest.java
@@ -1,11 +1,19 @@
 package com.jenkins.newtest;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.junit.After;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 
 public class BaseTest {
     @BeforeTest
     public void beforeTest() {
         Configuration.remote = "http://localhost:4444/wd/hub";
+    }
+
+    @AfterTest
+    public void afterTest(){
+        Selenide.closeWebDriver();
     }
 }
